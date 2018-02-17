4c3241bace03bf387cf1dcfba0aed3d26347ae01
Jahirul Rayhan <johir.rayhan01@gmail.com>
2018-02-17 16:43:52 +0000

    redux first example

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("webpack cccc");
+"use strict"
+import {createStore} from 'redux';
+//STEP 3 define reducers
+const reducer = function(state =0, action){
+  switch (action.type) {
+    case "INC":
+      return state + action.payload;
+      break;
+  }
+  return state
+}
+
+// STEP 1 create the store
+const store = createStore(reducer);
+
+store.subscribe(function(){
+  console.log('current state is: '+ store.getState());
+})
+
+//STEP 2 create and dispatch action
+store.dispatch({type:"INC", payload:1})
+store.dispatch({type:"INC", payload:1})
+store.dispatch({type:"INC", payload:1})
